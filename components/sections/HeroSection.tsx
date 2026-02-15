127128fa73618db89adf241d2adb8869f5eb5039
Arjun259194 <arjun259194@gmail.com>
2026-02-15 15:43:39 +0530

    spelling checked, title italic

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -9,8 +9,8 @@ const HeroSection = (props: Props) => {
       <div style={{
         backgroundImage: "url('/Hero-BG.png')"
       }} className="flex flex-col text-center h-11/12 text-gray-100 pt-20 bg-no-repeat bg-top bg-cover rounded-2xl items-center space-y-5">
-        <h1 className="text-6xl font-semibold ">Stop Translating.<br />Start Speaking</h1>
-        <p className="w-[45ch] text-xl">Master Englisj conversation with expert traners and proven methods that deliver real results</p>
+        <h1 className="text-6xl italic font-semibold ">Stop Translating.<br />Start Speaking</h1>
+        <p className="w-[45ch] text-xl">Master English conversation with expert traners and proven methods that deliver real results</p>
         <div className="flex space-x-5">
           <Button>
             <span>Get Started</span> <ChevronRight size={16} />
